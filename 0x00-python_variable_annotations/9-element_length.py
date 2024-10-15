bd16c7d737783aba2b3eab09ff9daf5aa642201c
Emmanuel Ohwoka <emmyprime2015@gmail.cocm>
2024-10-15 15:22:50 +0000

    A function that takes a list of any object and return a tuples

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -5,19 +5,19 @@ tuples with elements and their corresponding lengths.
 """
 
 
-from typing import List, Tuple, Sequence
+from typing import List, Tuple, Sequence, Any
 
 
-def element_length(lst: Sequence[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Sequence[Any]) -> List[Tuple[Any, int]]:
     """
     A type-annotated function that takes a list of strings and returns
     a list of tuples where each tuple contains an element and its length.
 
     Args:
-        lst (Sequence[str]): A sequence of strings.
+        lst (Sequence[Any]): A sequence of strings.
 
     Returns:
-        List[Tuple[str, int]]: A list of tuples, each containing
+        List[Tuple[Any, int]]: A list of tuples, each containing
         a string and its corresponding length.
     """
     return [(i, len(i)) for i in lst]
