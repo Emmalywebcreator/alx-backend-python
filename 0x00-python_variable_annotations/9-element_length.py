9a15669902cbe7a8346c68492071b53a8d46befb
Emmanuel Ohwoka <emmyprime2015@gmail.cocm>
2024-10-15 15:46:10 +0000

    A function that takes a list of any object and return a tuples

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,23 +1,15 @@
 #!/usr/bin/env python3
 """
-This module provides a function that returns a list of
-tuples with elements and their corresponding lengths.
+Duck type iterable object
+Annotate a function param and return values
+with appropriate types
 """
-
-
-from typing import List, Tuple, Interable, Sequence
+from typing import Sequence, Iterable, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
-    A type-annotated function that takes a list of strings and returns
-    a list of tuples where each tuple contains an element and its length.
-
-    Args:
-        lst (Sequence[Any]): A sequence of strings.
-
-    Returns:
-        List[Tuple[Any, int]]: A list of tuples, each containing
-        a string and its corresponding length.
+    Annotate function param
+    element_length
     """
     return [(i, len(i)) for i in lst]
