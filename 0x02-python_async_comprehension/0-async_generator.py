0e33370e1a1defb6418805786e6dc49515a90c5f
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-10-18 12:19:50 -0700

    async generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -17,6 +17,6 @@ async def async_generator() -> Generator[float, None, None]:
     Yields:
         float: Random numbet between 0 and 10.
     """
-    for _ in range(10):
+    for i in range(10):
         await asyncio.sleep(1)
         yeild random.uniform(0, 10)
