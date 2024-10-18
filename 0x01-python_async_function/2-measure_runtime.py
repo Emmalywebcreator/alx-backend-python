9246088f580a4216a2bb00326a27cfddd5839c0a
Emmanuel Ohwoka <emmyprime2015@gmail.cocm>
2024-10-18 00:11:54 +0000

    Measure average runtime

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -22,7 +22,7 @@ def measure_time(n: int, max_delay: int) -> float:
     """
 
     start_time = time.perf_counter()
-    async.run( wait_n(n, max_delay))
+    asyncio.run( wait_n(n, max_delay))
     end_time = time.perf_counter()
     total_time = end_time - start_time
     return total_time / n
