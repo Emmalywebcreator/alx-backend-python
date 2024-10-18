7cdbdc2daebc1c169b08b41c7be0b2a333cfd73b
Emmanuel Ohwoka <emmyprime2015@gmail.cocm>
2024-10-18 00:04:00 +0000

    Measure average runtime

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+This module measure the average runtime of wait_n coroutine
+"""
+
+
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measure the total time ti takes to execute wait_n coroutine
+    and returns the average time.
+
+    args:
+        n (int): Number of times to spawn wait_random
+        max_delay (int): Maximum delay for each wait_random
+
+    Returns:
+        float: Average runtime for each execution of wait_n
+    """
+
+    start_time = time.perf_counter()
+    await wait_n(n, max_delay)
+    end_time = time.perf_counter()
+    total_time = end_time - start_time
+    return total_time / n
