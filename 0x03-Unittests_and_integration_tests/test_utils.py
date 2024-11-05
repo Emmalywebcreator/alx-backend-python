3a255e63e872f9e0030012d3d1c078f11ff366f8
Emmanuel Ohwoka <emmyprime2015@gmail.cocm>
2024-11-05 03:13:43 +0000

    parametirization

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -13,7 +13,8 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(
-            self, nested_map: Mapping[str, Any], path: Tuple[str, ...], expected: Any
+            self, nested_map: Mapping[str, Any], path: Tuple[str, ...],
+            expected: Any
     ) -> None:
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
