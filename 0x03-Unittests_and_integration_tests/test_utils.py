47c08e699ec69f6356dd9b1f8e44f026bc175a67
Emmanuel Ohwoka <emmyprime2015@gmail.cocm>
2024-11-08 14:29:08 +0000

    keyerror

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -28,6 +28,21 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(
+        self, nested_map: Mapping[str, Any], path: Tuple[str, ...]
+    ) -> None:
+        """
+        Test access_nested_map raises KeyError with expected message
+        for invalid path.
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"'{path[-1]}'")
+
 
 if __name__ == '__main__':
     unittest.main()
